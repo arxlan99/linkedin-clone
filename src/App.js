2d9c70fbf9515250a76e7fabdd32e270375ba541
Özkan Arslan <ozkan.arslan.321@gmail.com>
2021-12-23 21:20:36 +0300

    Fix spinner component

--- src/App.js
+++ src/App.js
@@ -3,15 +3,33 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getUserAuth } from "./actions";
 import { connect } from "react-redux";
+import { auth } from "./firebase";
+import styled from "styled-components";
 
 function App(props) {
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     props.getUserAuth();
   }, [props]);
-  console.log(process.env.REACT_APP_FIREBASE_API_KEY);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(async (user) => {
+      setLoading(false);
+    });
+    return unsubscribe;
+  }, []);
+
+  if (loading) {
+    return (
+      <Spinner>
+        <img src="/images/spinner.svg" alt="" />
+      </Spinner>
+    );
+  }
 
   return (
     <BrowserRouter>
@@ -25,6 +43,18 @@ function App(props) {
     </BrowserRouter>
   );
 }
+
+const Spinner = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-size: 2rem;
+  font-weight: bold;
+  padding: 1rem;
+  border-radius: 5px;
+  z-index: 999;
+`;
 
 const mapStateToProps = (state) => {
   return {
@@ -40,26 +70,3 @@ const mapDispatchToProps = (dispatch) => {
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
-
-// eslint-disable-next-line no-lone-blocks
-{
-  /* <BrowserRouter>
-      {props.loading ? (
-        <Spinner>Loading...</Spinner>
-      ) : (
-        <Routes>
-          {!props.user ? (
-            <Route path="*" element={<Login />} />
-          ) : (
-            <Fragment>
-              <Route path="/Login" element={<Login />} />
-              <Route path="/" element={<Home />}>
-                <Route path="/Home" />
-              </Route>
-              <Route path="*" element={<NotFound />} />
-            </Fragment>
-          )}
-        </Routes>
-      )}
-    </BrowserRouter> */
-}
